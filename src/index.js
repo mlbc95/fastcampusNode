5e057df3a1368e900b2820a0609a295f742d84be
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2017-06-24 17:09:00 -0500

    Apply to Router rendering instead of default App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import FastCampusContainer from './containers/FastCampusContainer';
+import registerServiceWorker from './registerServiceWorker'; 
+import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <FastCampusContainer />,
+  document.getElementById('root')
+);
+registerServiceWorker(); 
