ec00c1b7660599cd5d4b9256116c16d0fe4229c1
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2017-06-24 17:06:30 -0500

    Added basic component boilerplates

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => (
+  <nav>
+    <ul>
+      <li><NavLink exact to="/">Home</NavLink></li>
+    </ul>    
+  </nav>
+);
+
+export default NavBar;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <i className="material-icons icn-error">error_outline</i>
+    <h2>Page Not Found</h2>
+  </div>
+);
+
+export default NotFound;
