6676f0df0fef33fadb7b48567cb0aa0e3b98527a
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2017-06-29 22:45:07 -0500

    Pseudo navbar test implementation

---
+++ src/containers/FastCampusContainer/FastCampusContainer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+// App components
+import NavBar from '../../components/NavBar/NavBar'
+import Home from '../../components/Home/Home'
+import Base from '../../components/Base/Base'
+import NotFound from '../../components/NotFound/NotFound'
+import LoginPage from '../LoginPage/LoginPage'
+import SignUpPage from '../SignUpPage/SignUpPage'
+
+const FastCampusContainer = () => (
+  <BrowserRouter>
+    <div style={{ height: '100%' }}>
+      {/*<NavBar />*/}
+      <Base />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/signup' component={SignUpPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+export default FastCampusContainer
+
+
+
+
