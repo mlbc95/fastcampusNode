d27610e3b55386fa13a6978c99161db8ba786300
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2017-07-05 23:26:44 -0500

    Minor style changes

--- src/styles.js
+++ src/styles.js
@@ -3,8 +3,10 @@ import { grey600 } from 'material-ui/styles/colors'
 
 const styles = {
   navigation: {
+    fontSize: 15,
     fontWeight: typography.fontWeightLight,
     color: grey600,
+    paddingBottom: 15,
     display: 'block'
   },
   title: {
