fff826a2a3b53e78e835d91a0ee74fbf94a777e2
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2017-08-26 14:36:17 -0500

    add big-calendar styles

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import registerServiceWorker from './registerServiceWorker'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import 'font-awesome/css/font-awesome.css'
 import './css/index.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 // Material UI theme wrapper
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
