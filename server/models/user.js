e214df521ee7adcd48eab690debb467e181e404a
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-09 17:31:31 -0500

    Re-did the module.export changes in the server/user.js so now sign up should work

--- server/models/user.js
+++ server/models/user.js
@@ -57,71 +57,66 @@ const UserSchema = mongoose.Schema({
 
 const User = module.exports = mongoose.model('User', UserSchema)
 
-module.exports = {
-  // checks if user's password is correct or not 
-  comparePassword: (candidatePassword, hash, callback) => {
-    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-      if (err) throw err
-      callback(null, isMatch)
-    })
-  },
+// returns user information by userID
+module.exports.getUserById = function (id, callback) {
+  User.findById(id, callback)
+}
 
-  // returns information for a specific user 
-  getUserByUsername: (username, callback) => {
-    const query = {
-      username: username
+// returns information for a specific user 
+module.exports.getUserByUsername = function (username, callback) {
+  const query = {username: username}
+  User.findOne(query, callback)
+}
+
+// Adds user to the mongoDb
+module.exports.addUser = function (newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.log('error', err)
+    } else {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) { console.log('error', err) }
+        newUser.password = hash
+        newUser.save(callback)
+      })
     }
-    User.findOne(query, callback)
-  },
+  })
+}
 
-  // Adds user to the mongoDb
-  addUser: (newUser, callback) => {
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) {
-        console.log('error', err)
-      } else {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) {
-            console.log('error', err)
-          }
-          newUser.password = hash
-          newUser.save(callback)
-        })
-      }
-    })
-  },
+// checks if user's password is correct or not 
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if (err) throw err
+    callback(null, isMatch)
+  })
+}
 
-  // returns user information by userID
-  getUserById: (id, callback) => {
-    User.findById(id, callback)
-  },
+// Updates user info 
+module.exports.updateUser = function (id, updatedUser, callback) {
+  console.log(updatedUser)
+  User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(id)}, updatedUser, callback)
+}
 
-  // Updates user info
-  updateUser: (id, updatedUser, callback) => {
-    console.log(updatedUser)
-    User.findByIdAndUpdate({
-      _id: mongoose.Types.ObjectId(id)
-    }, updatedUser, callback)
-  },
+// Gives the caller the enitre list of users 
+module.exports.getAllUsers = function (callback) {
+  User.find({}, {password: 0, _id: 0}, callback)
+}
 
-  getAllUsers: (callback) => {
-    User.find({}, {
-      password: 0,
-      _id: 0
-    }, callback)
-  },
+// --------------------- Classes --------------------------------//
 
-  // --------------------- Classes --------------------------------//
+// Addes class to the user's info 
+module.exports.addClasses = function (id, classes, classback) {
+  console.log(classes)
+  User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(id), classes, classback})
+}
 
-  // Addes class to the user's info 
-  addClasses: (id, classes, classback) => {
-    console.log(classes)
-    User.findByIdAndUpdate({
-      _id: mongoose.Types.ObjectId(id),
-      classes,
-      classback
-    })
-  }
+module.exports.updateUser = function (id, updatedUser, callback) {
+  console.log(updatedUser)
+  User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(id)}, updatedUser, callback)
+}
+
+module.exports.getAllUsers = function (callback) {
+  User.find({}, {password: 0, _id: 0}, callback)
 }
 
 module.exports.getAllUsers = function (callback) {
