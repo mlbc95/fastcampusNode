501640f9e6dfd8fab89056ff6af9fe9021287df6
Boris Pallares <borispallares@hotmail.com>
2017-09-09 11:30:55 -0500

    Settings page First section done. backend does't update

--- client/src/containers/SettingsPage/index.js
+++ client/src/containers/SettingsPage/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Base from '../../components/Base'
-import {settingsData} from '../../data'
 import TextField from 'material-ui/TextField'
 import Settings from '../../components/Settings/Settings'
 import EditCard from '../../components/Settings/EditCards'
@@ -10,31 +9,53 @@ class SettingsPage extends Component {
   constructor (props) {
     var token = localStorage.getItem('token')
     super(props)
-
-    axios.get('auth/profile', {
-      headers: {Authorization: token}
+    this.state = {
+      fName: 'Na',
+      lName: 'NA',
+      school: 'NA',
+      gps: false,
+      edit: false
+    }
+    var self = this // this means something different in axios
+    axios.get('user/profile', { headers: {Authorization: token}
     }).then(function (response) {
       var user = response.data.user
-      console.log(user)
+      self.setState({
+        fName: user.fName,
+        lName: user.lName,
+        school: user.school
+      })
     }).catch(function (error) {
       console.log(error)
     })
-    this.state = {
-      fName: settingsData.fName,
-      lName: settingsData.lName,
-      school: settingsData.school,
-      gps: settingsData.gps,
-      edit: false
-    }
   }
-
-  handleClick () {
-    this.setState({
-      edit: !this.state.edit
-    })
-  }
-
   render () {
+    const handleClick = async() => {
+      if (this.state.edit) {
+        axios.post('user/updateUser', this.state, { headers: {Authorization: localStorage.getItem('token')}
+        }).then(function (response) {
+          console.log(response)
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
+      this.setState({
+        edit: !this.state.edit
+      })
+    }
+    const editValues = (event) => {
+      switch (event.target.id) {
+        case '1':
+          this.setState({fName: event.target.value})
+          break
+        case '2':
+          this.setState({lName: event.target.value})
+          break
+        case '3':
+          this.setState({school: event.target.value})
+          break
+      }
+    }
     return (
       <Base
         title='Settings'
@@ -45,28 +66,34 @@ class SettingsPage extends Component {
             title='Personal'
             subtitle='Email / Password'
             edit={this.state.edit}
-            handleClick={() => this.handleClick()}
+            handleClick={() => handleClick()}
             data={
               this.state.edit ? (
                 <div>
                   <h1>Fill the fields that are to be updated</h1>
                   <TextField
                     className='text'
-                    hintText={settingsData.fName}
+                    hintText={this.state.fName}
                     floatingLabelText='First Name'
                     floatingLabelFixed
+                    id='1'
+                    onChange={editValues}
                   /> <br />
                   <TextField
                     className='text'
-                    hintText={settingsData.lName}
+                    hintText={this.state.lName}
                     floatingLabelText='Last Name'
                     floatingLabelFixed
+                    id='2'
+                    onChange={editValues}
                   /> <br />
                   <TextField
                     className='text'
-                    hintText={settingsData.school}
+                    hintText={this.state.school}
                     floatingLabelText='School'
                     floatingLabelFixed
+                    id='3'
+                    onChange={editValues}
                   />
                 </div>
 
