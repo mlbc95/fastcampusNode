23cbecb09e31f36b80b888906f398917c6aa0953
Mohamed Langi <mlnp3@mail.umsl.edu>
2017-09-12 21:37:12 -0500

    new changes

--- server/models/user.js
+++ server/models/user.js
@@ -60,68 +60,6 @@ const UserSchema = mongoose.Schema({
 
 const User = module.exports = mongoose.model('User', UserSchema)
 
-<<<<<<< HEAD
-// returns user information by userID
-module.exports.getUserById = function (id, callback) {
-  User.findById(id, callback)
-}
-
-// returns information for a specific user 
-module.exports.getUserByUsername = function (username, callback) {
-  const query = {username: username}
-  User.findOne(query, callback)
-}
-
-// Adds user to the mongoDb
-module.exports.addUser = function (newUser, callback) {
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      console.log('error', err)
-    } else {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) { console.log('error', err) }
-        newUser.password = hash
-        newUser.save(callback)
-      })
-    }
-  })
-}
-
-// checks if user's password is correct or not 
-module.exports.comparePassword = function (candidatePassword, hash, callback) {
-  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-    if (err) throw err
-    callback(null, isMatch)
-  })
-}
-
-// Updates user info 
-module.exports.updateUser = function (id, updatedUser, callback) {
-  console.log(updatedUser)
-  User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(id)}, updatedUser, callback)
-}
-
-// Gives the caller the enitre list of users 
-module.exports.getAllUsers = function (callback) {
-  User.find({}, {password: 0, _id: 0}, callback)
-}
-
-// --------------------- Classes --------------------------------//
-
-// Addes class to the user's info 
-module.exports.addClasses = function (id, classes, classback) {
-  console.log(classes)
-  User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(id), classes, classback})
-}
-
-module.exports.updateUser = function (id, updatedUser, callback) {
-  console.log(updatedUser)
-  User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(id)}, updatedUser, callback)
-}
-
-module.exports.getAllUsers = function (callback) {
-  User.find({}, {password: 0, _id: 0}, callback)
-=======
 // checks if user's password is correct or not 
 module.exports.comparePassword = function (candidatePassword, hash, user, callback) {
   bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
@@ -134,7 +72,6 @@ module.exports.comparePassword = function (candidatePassword, hash, user, callba
       User.findByIdAndUpdate({_id: mongoose.Types.ObjectId(user._id)}, {lastLogin: new Date()}, callback)
     }
   })
->>>>>>> develop/api
 }
 
 module.exports.getAllUsers = function (callback) {
