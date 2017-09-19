bfd0519de550e51cccd32cfe04b213f7b15d266b
matt <matt.bernardini@gmail.com>
2017-09-19 18:10:11 -0500

    added course validation

--- server/models/tutor.js
+++ server/models/tutor.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const lodash = require('lodash')
+const typeCheck = require('type-check').typeCheck
 
 const TutorSchema = mongoose.Schema({
   fName: {
@@ -63,5 +64,36 @@ module.exports.validateLName = function (tutor, obj) {
     obj.lName = 'Please enter a valid last name'
   } else {
     // Clean up last name
+  }
+}
+
+module.exports.validateCourses = function (tutor, obj) {
+  // If the object is present and equal to an array we process further
+  if ('courses' in tutor && !lodash.isArray(tutor.courses)) {
+    // The object exists but is in an invalid format
+    obj.courses = 'Please enter classes in a valid array'
+  } else if ('courses' in tutor && lodash.isArray(tutor.courses)) {
+    // Is present and in an array loop through array
+    lodash.forEach(tutor.courses, function (value) {
+      // Loop through object and check values
+      lodash.forIn(value, function (key, val) {
+        // Switch on key and validate each part
+        switch (key) {
+          case 'courseNumber':
+            if (!typeCheck('Number', value)) {
+              obj.courses.courseNumber = 'Please enter a valid number'
+              break
+            }
+            break
+          case 'className':
+            const regex = /((\w+) ?){1,}/
+            if (!regex.test(value)) {
+              obj.courses.courseName = 'Please enter a valid classname'
+              break
+            }
+            break
+        }
+      })
+    })
   }
 }
