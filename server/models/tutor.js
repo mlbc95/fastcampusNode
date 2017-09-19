9e4fdd38ae06244d5c4395230714ae2ae6b26089
matt <matt.bernardini@gmail.com>
2017-09-19 18:07:33 -0500

    fname and lname validation added

--- server/models/tutor.js
+++ server/models/tutor.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const lodash = require('lodash')
 
 const TutorSchema = mongoose.Schema({
   fName: {
@@ -42,3 +44,24 @@ const TutorSchema = mongoose.Schema({
 })
 
 const Tutor = module.exports = mongoose.model('Tutor', TutorSchema)
+
+module.exports.validateFName = function (tutor, obj) {
+  console.log('in first name validator')
+  // If the fName is in the object AND doesnt pass the regex test
+  if ('fName' in tutor && !validator.isAlpha(tutor.fName.trim())) {
+    // Insert failure return message for processing by frontend
+    obj.fName = 'Please enter a valid first name'
+  } else {
+    // Clean up fname
+  }
+}
+
+module.exports.validateLName = function (tutor, obj) {
+  // If the lName is in user object and doesnt pass the regex test
+  if ('lName' in tutor && !validator.isAlpha(tutor.lName.trim())) {
+    // Insert failure logic
+    obj.lName = 'Please enter a valid last name'
+  } else {
+    // Clean up last name
+  }
+}
