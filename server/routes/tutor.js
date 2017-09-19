1e6902279a08e046639e2e863556195d332c906c
matt <matt.bernardini@gmail.com>
2017-09-19 18:52:49 -0500

    created tutor routes page

---
+++ server/routes/tutor.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const Tutor = require('../models/tutor.js')
+const router = new express.Router()
+/*
+  This file contains api calls for Tutor
+*/
+
+router.post('/addTutor', (req, res) => {
+  
+})
+module.exports = router
