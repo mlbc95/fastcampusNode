548fd227166ca371fdcba39cf7f6428a1e859000
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-22 22:44:09 -0500

    Done

--- server/classes.js
+++
@@ -1,9 +0,0 @@
-var csv2json = require('csv2json')
-var fs = require('fs')
-
-fs.createReadStream('FS17 PDF of Classes.csv')
-  .pipe(csv2json({
-    // Defaults to comma. 
-    separator: ','
-  }))
-  .pipe(fs.createWriteStream('data.json'))
--- server/routes/classes.js
+++ server/routes/classes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = new express.Router()
 const lodash = require('lodash')
 const fS17Courses = require('../data.json')
-const typeCheck = require('type-check').typeCheck
 
 router.get('/allCourses', (req, res, next) => {
   return res.json(fS17Courses)
@@ -20,24 +19,46 @@ router.get('/allCourses', (req, res, next) => {
           Fail:
             none
           Success:
-            arraylist of Field names ex : [Accounting, .....]
+            arraylist of field names ex : [Accounting, .....]
 */
 router.get('/fields', (req, res, next) => {
   var fieldList = []
   lodash.forEach(fS17Courses, function (value, key) {
-    if (fS17Courses[key].Field !== undefined) {
-      fieldList.push(fS17Courses[key].Field)
+    if (fS17Courses[key].field !== undefined) {
+      fieldList.push(fS17Courses[key].field)
     }
   })
   return res.json(fieldList)
 })
 
+/*
+  This get takes in a header field with a field name and returns list of courses for that field
+   /class/fields | get | Returns the list of 
+        Content-Type : application/json
+        Information Expected
+          field ex: field : Accounting in the req header
+        Returns:
+          Fail:
+            if req is empty 
+              {success: false, courseList: 'No course Selected'}
+            if type in filed
+              {success: false, courseList: 'No courses found for selected field.'}
+          Success:
+            {sucess: true, courseList: {"courseNumber" : ....}}
+*/
 router.get('/getCourse', (req, res, next) => {
+  if (!req.get('field')) {
+    return res.json({success: false, courseList: 'No course Selected'})
+  }
+  var sent = false
   lodash.forEach(fS17Courses, function (value, key) {
-    if (fS17Courses[key].Field === req.body.Field) {
-      return res.json(fS17Courses[key].Field)
+    if (fS17Courses[key].field === req.get('field')) {
+      sent = true
+      return res.json({sucess: true, courseList: fS17Courses[key].courses})
     }
   })
-  return res.json({success: false, courseList: 'No courses found for selected Field.'})
+  if (!sent) {
+    return res.json({success: false, courseList: 'No courses found for selected field.'})
+  }
 })
 module.exports = router
