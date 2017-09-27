6fcaadfce3896ebe58e2e652337e818e4bfb7092
matt <matt.bernardini@gmail.com>
2017-09-27 11:06:01 -0500

    deleted extra files that were no longer needed, made the schema of Teacher and Student match what we had established last night.

---
+++ src/models/Teacher.ts
@@ -0,0 +1,29 @@
+import * as bcrypt from "bcrypt-nodejs";
+import * as crypto from "crypto";
+import * as mongoose from "mongoose";
+import { UserModel, User } from "./User";
+const options = {discriminatorKey: "role", timestamps: true};
+export interface TeacherModel extends UserModel, mongoose.Document  {
+    status: string;
+    officeHours: DayOfWeek[];
+}
+
+export type DayOfWeek = {
+    day: string,
+    hours: string[],
+    office: Office
+};
+
+export type Office = {
+    building: string,
+    roomNumber: string
+};
+
+
+export const teacherSchema = new mongoose.Schema({
+  status: String,
+  officeHours: Array
+}, options);
+
+const Teacher = User.discriminator("Teacher", teacherSchema);
+export default Teacher;
