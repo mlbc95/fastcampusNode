f0a5801b20400100d60aaf68020451c6bd20fe5a
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-30 14:03:11 -0500

    Trying something new

--- src/server.ts
+++ src/server.ts
@@ -102,21 +102,8 @@ app.use((req, res, next) => {
 //   next();
 // });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
 
-  // intercepts OPTIONS method
-  if ("OPTIONS" === req.method) {
-    // respond with 200
-    res.send(200);
-  }
-  else {
-  // move on
-    next();
-  }
-});
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
 
 app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
@@ -124,8 +111,6 @@ app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
   res.send(200);
 });
-
-app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
 
 
 // swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
