00a16bc07372e7508b1ee2ac136ceb27a6350753
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-30 14:51:45 -0500

    Still not working for clinet

--- src/server.ts
+++ src/server.ts
@@ -115,8 +115,7 @@ app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-  // res.send(200);
-  next();
+  res.send(200);
 });
 
 
