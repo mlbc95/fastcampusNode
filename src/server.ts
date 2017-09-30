25673840949366ba89caf59d4cb9993aa1923005
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-30 14:03:20 -0500

    Trying something new

--- src/server.ts
+++ src/server.ts
@@ -109,7 +109,7 @@ app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-  res.send(200);
+  // res.send(200);
 });
 
 
