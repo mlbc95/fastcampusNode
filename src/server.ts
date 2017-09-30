80b138857189c9f432d380ba6c1ca78860c31762
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-30 14:48:32 -0500

    Still not working for clinet

--- src/server.ts
+++ src/server.ts
@@ -102,6 +102,11 @@ app.use((req, res, next) => {
 //   next();
 // });
 
+// app.use(function (req, res, next){
+//   cors();
+//   next();
+// });
+
 
 app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
 // app.use(cors());
@@ -111,6 +116,7 @@ app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
   // res.send(200);
+  next();
 });
 
 
