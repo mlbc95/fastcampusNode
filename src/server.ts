7444415d8960aff2f422a7bdf785c82d5f078aec
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-30 13:36:54 -0500

    it broke heroku

--- src/server.ts
+++ src/server.ts
@@ -95,12 +95,12 @@ app.use((req, res, next) => {
 
 
 // Allow CORS
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "DELETE");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
