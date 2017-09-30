0b5269a9bc891c9a1dfae8b9df94d54a0b7bba4a
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-09-30 14:44:54 -0500

    Messign about to see if I can fix this on heroku

--- src/server.ts
+++ src/server.ts
@@ -104,14 +104,14 @@ app.use((req, res, next) => {
 
 
 app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
-app.use(cors());
-// app.options("/*", function(req, res, next){
-//   res.header("Access-Control-Allow-Credentials");
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-//   res.send(200);
-// });
+// app.use(cors());
+app.options("/*", function(req, res, next){
+  res.header("Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+  // res.send(200);
+});
 
 
 // swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
