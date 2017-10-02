b270e1e594034cded63db2e6da5a51999c1232dc
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-10-02 18:11:10 -0500

    Changing CORS from app.use to app.options

--- src/server.ts
+++ src/server.ts
@@ -95,11 +95,11 @@ app.use((req, res, next) => {
 
 
 // Allow CORS
-app.use("/*", function(req, res, next){
+app.options("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-  next();
+  res.send(200);
 });
 
 
