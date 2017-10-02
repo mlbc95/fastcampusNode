4f0bf8c5f57b92e29bc5b9e1391abe2cc69ce633
Harprabh Sangha <hsswx7@mail.usml.edu>
2017-10-02 17:27:18 -0500

    Removed a cors error

--- src/server.ts
+++ src/server.ts
@@ -97,7 +97,6 @@ app.use((req, res, next) => {
 // Allow CORS
 app.use("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
   next();
