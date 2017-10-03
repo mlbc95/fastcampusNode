9b3612f9ddb43b804537df6ce614ea21d3ce0247
matt <matt.bernardini@gmail.com>
2017-10-02 20:52:07 -0500

    slight change

--- src/server.ts
+++ src/server.ts
@@ -100,7 +100,7 @@ app.use("/*", function(req, res, next){
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
   if ("OPTIONS" === req.method) {
-    res.send(200);
+    return res.send(200);
   }
   next();
 });
