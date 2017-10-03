2c1353b59b2aba777376b419f98b79984aba14b2
matt <matt.bernardini@gmail.com>
2017-10-03 11:04:48 -0500

    testing

--- src/server.ts
+++ src/server.ts
@@ -99,7 +99,11 @@ app.use("/*", function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   // res.header("ALLOW", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-  next();
+  if ("OPTIONS" === req.method) {
+    return res.send(200);
+  } else {
+    next();
+  }
 });
 
 
